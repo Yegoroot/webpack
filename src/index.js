0eba7dbc989291aba17bfa5936973c620f7cd087
Yegor <yegor@iMac.local>
2019-04-29 18:13:03 +0300

    init 2 step

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import _ from "lodash";
-import "./style.css";
-import Img from "./img.png";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  const myImg = new Image();
-  myImg.src = Img;
-
-  element.appendChild(myImg);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,9 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); //
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
-    ],
   },
 };
