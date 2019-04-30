3026d6ea22cc50b4db54e54e835692f480700815
Yegor <yegor@iMac.local>
2019-04-30 13:15:56 +0300

    webpack guide - development

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js",
+  },
   devtool: "inline-source-map",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  devServer: {
+    contentBase: "./dist",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
