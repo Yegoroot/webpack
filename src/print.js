af10207d97962a45461e0efde5c58cf049914bb9
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2019-07-09 21:02:34 +0300

    added new changes because previos commit is useless

--- src/print.js
+++ src/print.js
@@ -1,3 +1,1 @@
-export default function printMe() {
-  console.log("Updating print.js...");
-}
+console.log('-=-4456');
--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,57 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
+
+
 module.exports = {
   mode: "development",
   entry: {
     app: "./src/index.js",
-  },
-  devtool: "inline-source-map",
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Development",
-    }),
-    new webpack.HotModuleReplacementPlugin(), // встроенный плагин
-  ],
-  devServer: {
-    contentBase: "./dist",
-    hot: true,
-  },
+  }
+  ,
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: "inline-source-map",
+
+  devServer: {
+    contentBase: './public',
+    watchContentBase: true,
+    hot: true, // если инлайново добавить --hot то можно не указывать в конфиге
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(), // clean dist
+    new HtmlWebpackPlugin({ 
+      template: './public/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(), // если инлайново добавить --hot то можно не указывать в конфиге
+  ],
+   module: {
+    rules: [
+    {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/, 
+        use: [
+          'file-loader'
+        ]
+      }
+
+    ]
+  },
 };
+
+
